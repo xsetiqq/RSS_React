97e966d025383381199c3840f4eaae07c570176d
Tsimafei <xsetiqq@gmail.com>
2025-02-07 13:16:32 +0100

    feat: test husky

--- rs-react-app/src/main.tsx
+++ rs-react-app/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+
+
+
+
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
